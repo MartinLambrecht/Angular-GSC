dfea6426cdca3407c8be0ac9566ecc5caaa2670a
MartinLambrecht <69395872+MartinLambrecht@users.noreply.github.com>
2022-09-12 19:43:33 -0300

    Solution Ej2

--- ejercicio02/src/app/app.module.ts
+++ ejercicio02/src/app/app.module.ts
@@ -1,14 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: []
+	declarations: [AppComponent],
+	imports: [BrowserModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
