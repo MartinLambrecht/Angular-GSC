44866b5cd6c4543d6d571dfa02565379c8e9fd4d
MartinLambrecht <69395872+MartinLambrecht@users.noreply.github.com>
2022-10-08 13:32:22 -0300

    Solution Ej42

--- ejercicio42/src/app/app.module.ts
+++ ejercicio42/src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { ParentComponent } from './parent/parent.component';
+import { AppComponent } from "./app.component";
+import { ParentComponent } from "./parent/parent.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ParentComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, ParentComponent],
+	imports: [BrowserModule, HttpClientModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- ejercicio42/src/app/parent/parent.component.ts
+++ ejercicio42/src/app/parent/parent.component.ts
@@ -1,20 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { from, Observable } from "rxjs";
 
 @Component({
-  selector: 'app-parent',
-  templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css']
+	selector: "app-parent",
+	templateUrl: "./parent.component.html",
+	styleUrls: ["./parent.component.css"],
 })
 export class ParentComponent implements OnInit {
+	movieList!: Movie[]; // Corregir
 
-  movieList: any; // Corregir
+	constructor(private http: HttpClient) {}
 
-  constructor() { }
+	ngOnInit() {}
 
-  ngOnInit() {
-  }
+	makeRequest() {
+		this.http.get("https://swapi.dev/api/films/").subscribe((m: any) => {
+			this.movieList = m.results;
+		});
+	}
+}
 
-  makeRequest() {
-
-  }
+export interface Movie {
+	title: string;
+	episode_id: number;
+	opening_crawl: string;
+	director: string;
+	producer: string;
+	release_date: Date;
+	characters: string[];
+	planets: string[];
+	starships: string[];
+	vehicles: string[];
+	species: string[];
+	created: Date;
+	edited: Date;
+	url: string;
 }
