610dbee2ab8b7c05fc5d690fbc978fd94e9a3206
MartinLambrecht <69395872+MartinLambrecht@users.noreply.github.com>
2022-10-08 20:55:47 -0300

    Solution Ej44

--- ejercicio44/src/app/app.module.ts
+++ ejercicio44/src/app/app.module.ts
@@ -1,23 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { ParentComponent } from './parent/parent.component';
+import { AppComponent } from "./app.component";
+import { ParentComponent } from "./parent/parent.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ParentComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, ParentComponent],
+	imports: [BrowserModule, FormsModule, HttpClientModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
-
+export class AppModule {}
--- ejercicio44/src/app/parent/parent.component.ts
+++ ejercicio44/src/app/parent/parent.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-parent',
-  templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css']
+	selector: "app-parent",
+	templateUrl: "./parent.component.html",
+	styleUrls: ["./parent.component.css"],
 })
 export class ParentComponent implements OnInit {
+	appid = "69bc9bb1bbaa496601358677ea57a017";
+	forecast!: any;
+	city!: string;
 
-  appid = "69bc9bb1bbaa496601358677ea57a017";
-  forecast;
-  city;
+	constructor(private http: HttpClient) {}
 
-  constructor() { }
+	ngOnInit() {}
 
-  ngOnInit() {
-  }
+	makeRequest() {
+		const url =
+			"http://api.openweathermap.org/data/2.5/weather?q=" +
+			this.city +
+			"&APPID=" +
+			this.appid;
 
-  makeRequest() {
-    const url = "http://api.openweathermap.org/data/2.5/weather?q=" + this.city + "&APPID=" + this.appid;
-  }
-
+		this.http.get(url).subscribe((r: any) => {
+			console.log(r.weather);
+			this.forecast = r.weather[0].description;
+		});
+	}
 }
-
