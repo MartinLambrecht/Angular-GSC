0a2895c33344f83ab712e8bcbaf0c47fa7e4d984
MartinLambrecht <69395872+MartinLambrecht@users.noreply.github.com>
2022-10-08 12:13:55 -0300

    Solution Ej39

--- ejercicio39/src/app/movies/movies.component.ts
+++ ejercicio39/src/app/movies/movies.component.ts
@@ -1,27 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, ParamMap } from "@angular/router";
 
 @Component({
-  selector: 'app-movies',
-  templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.css']
+	selector: "app-movies",
+	templateUrl: "./movies.component.html",
+	styleUrls: ["./movies.component.css"],
 })
 export class MoviesComponent implements OnInit {
+	movies = [
+		{ name: "Star Wars", rating: "R", id: 1 },
+		{ name: "Jaws", rating: "PG", id: 2 },
+		{ name: "E.T.", rating: "R", id: 3 },
+		{ name: "Teen Wolf", rating: "G", id: 4 },
+		{ name: "Ferris Bueller's Day Off", rating: "G", id: 5 },
+	];
+	currentMovie: any;
 
-  movies = [
-    {name: "Star Wars", rating: 'R', id: 1},  
-    {name: "Jaws", rating: 'PG', id: 2},  
-    {name: "E.T.", rating: 'R', id: 3},  
-    {name: "Teen Wolf", rating: 'G', id: 4},  
-    {name: "Ferris Bueller's Day Off", rating: 'G', id: 5},  
-  ]
-  currentMovie: any;
-  
-  constructor(private route: ActivatedRoute) {
-  }
-  
-  ngOnInit() {
-  }
+	constructor(private route: ActivatedRoute) {}
 
+	ngOnInit() {
+		this.route.paramMap.subscribe((paramMap) => {
+			this.currentMovie = this.movies.find((m) => {
+				return m.id === +paramMap.get("id")!;
+			});
+		});
+	}
 }
-
