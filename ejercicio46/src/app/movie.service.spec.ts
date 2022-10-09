a544c945385091b63486e08f6011a7ad2350fbb8
MartinLambrecht <69395872+MartinLambrecht@users.noreply.github.com>
2022-10-08 23:11:28 -0300

    Solution Ej46

--- ejercicio46/src/app/movie.service.spec.ts
+++ ejercicio46/src/app/movie.service.spec.ts
@@ -1,14 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { MovieService } from './movie.service';
+import { MovieService } from "./movie.service";
 
-describe('MovieService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-  }));
+describe("MovieService", () => {
+	beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: MovieService = TestBed.get(MovieService);
-    expect(service).toBeTruthy();
-  });
+	it("should be created", () => {
+		const service: MovieService = TestBed.get(MovieService);
+		expect(service).toBeTruthy();
+	});
 
+	it("Should increment cast members size.", () => {
+		const service: MovieService = TestBed.get(MovieService);
+		const lengthBefore = service.castMembers.length;
+		service.addCastMember("Test", "Test");
+		const lengthAfter = service.castMembers.length;
+
+		expect(lengthAfter).toBe(lengthBefore + 1);
+	});
 });
