2e4b0b67f0598e04c8a3b751f9378fd4146be684
MartinLambrecht <69395872+MartinLambrecht@users.noreply.github.com>
2022-10-08 23:19:11 -0300

    Solution Ej47

--- ejercicio47/src/app/movie.service.spec.ts
+++ ejercicio47/src/app/movie.service.spec.ts
@@ -1,24 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { MovieService } from './movie.service';
-import { ActorService } from './actor.service';
+import { MovieService } from "./movie.service";
+import { ActorService } from "./actor.service";
 
-describe('MovieService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe("MovieService", () => {
+	beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: MovieService = TestBed.get(MovieService);
-    expect(service).toBeTruthy();
-  });
+	it("should be created", () => {
+		const service: MovieService = TestBed.get(MovieService);
+		expect(service).toBeTruthy();
+	});
 
-  it('adding a cast member increases the cast member count by one', () => {
-    const service: MovieService = TestBed.get(MovieService);
+	it("adding a cast member increases the cast member count by one", () => {
+		const service: MovieService = TestBed.get(MovieService);
 
-    let mockActorService;
-    // agregamos el codigo aqui
+		let mockActorService = TestBed.get(ActorService);
+		// agregamos el codigo aqui
+		spyOn(mockActorService, "canAddMovieToActor").and.returnValue(true);
 
-    service.addCastMember('Sylvester', 'Rambo', mockActorService);
-    expect(service.castMembers.length).toBe(1)    
-  });
-
+		service.addCastMember("Sylvester", "Rambo", mockActorService);
+		expect(service.castMembers.length).toBe(1);
+	});
 });
